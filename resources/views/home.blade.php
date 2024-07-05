0665453bf9d157aa14b19acf727f5682758404fc
andreasjulio11 <andreasjulio94@gmail.com>
2024-07-05 23:48:06 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,10 @@
     <title>Payment</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- <script type="text/javascript" src="https://app.midtrans.com/snap/snap.js" data-client-key="SET_YOUR_CLIENT_KEY_HERE">
+        </script> --}}
+    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
+        data-client-key="SET_YOUR_CLIENT_KEY_HERE"></script>
     <style>
         body {
             background-color: #f8f9fa;
@@ -22,7 +26,8 @@
             border: none;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
             transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
-            border-radius: 15px; /* Menggunakan border-radius untuk membuat card tidak kotak */
+            border-radius: 15px;
+            /* Menggunakan border-radius untuk membuat card tidak kotak */
         }
 
         .card:hover {
@@ -46,7 +51,8 @@
         .btn-primary {
             background-color: #ff6f61;
             border-color: #ff6f61;
-            border-radius: 20px; /* Menggunakan border-radius pada tombol */
+            border-radius: 20px;
+            /* Menggunakan border-radius pada tombol */
         }
 
         .btn-primary:hover {
@@ -72,7 +78,7 @@
 
 <body>
     <div class="container">
-        <h1 class="my-4 text-center text-primary">Payment</h1>
+        <h1 class="my-4 text-center text-primary">Payment Shop</h1>
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card">
@@ -80,8 +86,8 @@
                     <div class="card-body">
                         {{-- <h5 class="card-title">Durian Lokal</h5>
                         <p class="card-text">Durian lokal, rasanya manis dan ada pait-paitnya, dijamin wueeeenak.</p> --}}
-                        <form id="orderForm" action="/checkout" method="POST">
-                            @csrf
+                        {{-- <form id="orderForm" action="/order-payment-bypass" method="POST">
+                            @csrf --}}
                             {{-- <div class="mb-3">
                                 <label for="qty" class="form-label">Mau Pesan Berapa?</label>
                                 <input type="number" name="qty" class="form-control" id="qty"
@@ -90,13 +96,13 @@
                             <div class="mb-3">
                                 <label for="name" class="form-label">Nama Pelanggan</label>
                                 <input type="text" name="name" class="form-control" id="name"
-                                    placeholder="Masukkan nama anda!" required>
+                                    placeholder="Masukkan nama anda!" value="{{ $name }}" required readonly>
                             </div>
-                            <div class="mb-3">
+                            {{-- <div class="mb-3">
                                 <label for="phone" class="form-label">No Telp</label>
                                 <input type="text" name="phone" class="form-control" id="phone"
-                                    placeholder="Masukkan no hp!" required>
-                            </div>
+                                    placeholder="Masukkan no hp!" value="{{ $phone }}" required>
+                            </div> --}}
                             {{-- <div class="mb-3">
                                 <label for="address" class="form-label">Alamat</label>
                                 <textarea name="address" class="form-control" id="address" rows="3"
@@ -105,10 +111,11 @@
                             <div class="mb-3">
                                 <label for="total_price" class="form-label">Total Pembayaran</label>
                                 <input type="text" name="total_price" class="form-control" id="total_price"
-                                    placeholder="Masukkan Jumlah Pembayaran!" required>
+                                    placeholder="Masukkan Jumlah Pembayaran!" value="{{ $total_price }}" required>
                             </div>
+                            <input type="text" id="snap-token" hidden>
                             <button type="button" class="btn btn-primary w-100" id="checkoutButton">Checkout</button>
-                        </form>
+                        {{-- </form> --}}
                     </div>
                 </div>
             </div>
@@ -145,12 +152,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
-        document.getElementById('checkoutButton').addEventListener('click', function () {
+        var button = document.getElementById('checkoutButton');
+
+        document.getElementById('checkoutButton').addEventListener('click', function() {
             let isValid = true;
 
             // Validate each input field
-            const fields = ['name', 'phone', 'total_price'];
+            const fields = ['name', 'total_price'];
             fields.forEach(field => {
                 const input = document.getElementById(field);
                 if (input.value.trim() === '') {
@@ -162,22 +172,67 @@
             });
 
             if (isValid) {
+                button.disabled = true;
+                document.getElementById('checkoutButton').innerText = 'Proses'
                 document.getElementById('confirmName').innerText = 'Nama: ' + document.getElementById('name').value;
-                document.getElementById('confirmPhone').innerText = 'No Telp: ' + document.getElementById('phone').value;
-                // document.getElementById('confirmAddress').innerText = 'Alamat: ' + document.getElementById('address').value;
-                document.getElementById('confirmTotalPrice').innerText = 'Jumlah Pembayaran: ' + document.getElementById('total_price').value;
+                document.getElementById('confirmTotalPrice').innerText = 'Jumlah Pembayaran: ' + document
+                    .getElementById('total_price').value;
 
                 // Show the modal
-                const confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
-                confirmationModal.show();
+                // const confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
+                // confirmationModal.show();
+                name = document.getElementById('name').value
+                // phone = document.getElementById('phone').value
+                total_price = document.getElementById('total_price').value
+
+                $.ajax({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                    },
+                    type: 'POST',
+                    url: '{{ route('saveOrder') }}',
+                    data: {
+                        "_token": "{{ csrf_token() }}",
+                        name: name,
+                        total_price: total_price,
+                    },
+                    success: function(data) {
+                        snapToken = data.token
+                        orderCode = data.order_code
+                        document.getElementById('snap-token').value = snapToken
+                        window.snap.pay(snapToken, {
+                            onSuccess: function(result) {
+                                alert('Berhasil')
+                                
+                                button.disabled = false;
+                                document.getElementById('checkoutButton').innerText = 'Checkout'
+                                window.location.href = '/invoice/'+orderCode
+
+                            },
+                            onPending: function(result) {
+
+                            },
+                            onError: function(result) {
+
+                            },
+                            onClose: function(result) {
+
+                            },
+                        });
+                    },
+                    error: function(e) {
+                        console.log(e);
+                    }
+                });
+
             } else {
                 alert('Please fill in all fields');
             }
         });
 
-        document.getElementById('confirmButton').addEventListener('click', function () {
-            document.getElementById('orderForm').submit();
-        });
+        // document.getElementById('confirmButton').addEventListener('click', function() {
+        //     document.getElementById('orderForm').submit();
+        // });
     </script>
 </body>
 
